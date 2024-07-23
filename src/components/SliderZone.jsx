c92f73695e85ad747d9e30c9a94020c16ec40e0e
pearl <kjj0837@naver.com>
2024-07-23 12:04:58 +0900

    V2-MENUzONE 까지

--- src/components/SliderZone.jsx
+++ src/components/SliderZone.jsx
@@ -1,8 +1,33 @@
+import left_btnImg from '../img/left_btn.png';
+import right_btnImg from '../img/right_btn.png';
+
+import slider1 from '../img/1.jpg'
+import slider2 from '../img/2.jpg'
+import slider3 from '../img/3.jpg'
+import '../styles/SliderZone.css'
+
 export function SliderZone(){
   return (
-    <>
-    <p>SliderZone</p>
-    
-    </>
+    <div id="sliderZone">
+    <div id="sub_photo_bg">
+        <ul className="slider_panel">
+            <li className="slider_image"><img src={slider1} alt=""/></li>
+            <li className="slider_image"><img src={slider2} alt=""/></li>
+            <li className="slider_image"><img src={slider3} alt=""/></li>
+            {/* <!-- <li className="slider_image"><img src="img/1.jpg" alt=""></li>
+            <li className="slider_image"><img src="img/2.jpg" alt=""></li>
+            <li className="slider_image"><img src="img/3.jpg" alt=""></li> --> */}
+        </ul>
+        <div className="control_panel">
+            <div className="control_btn active"  data-index="0"></div>
+            <div className="control_btn" data-index="1"></div>
+            <div className="control_btn" data-index="2"></div>
+        </div>
+        <div className="direct_btn">
+            <div className="left_btn"><img src={left_btnImg} alt=""/></div>
+            <div className="right_btn"><img src={right_btnImg} alt=""/></div>
+        </div>
+    </div>
+</div>
   );
 }
--- src/components/TitleZone.jsx
+++ src/components/TitleZone.jsx
@@ -1,8 +1,21 @@
+import '../styles/TitleZone.css';
+import logoImg from '../img/logo.jpg';
 export function TitleZone(){
   return (
-    <>
-    <p>TitleZone</p>
-    
-    </>
+    // <!-- 타이틀Zone시작 -->
+    <div id="titleZone">
+        <div id="gnb">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Join</a></li>
+                <li><a href="#">Mypage</a></li>
+            </ul>
+        </div>
+        <div id="logo">
+            <img src={logoImg} alt=""/>
+        </div>
+    </div>
+    // <!-- 타이틀Zone끝 -->
   );
 }
